82b9fc708b886c440cf7c9dcdf1fb143aa1701b5
Sadhuram Agarwal <sadhuramyk7@gmail.com>
2026-05-15 18:40:36 +0530

    HF Spaces deployment

--- backend/run.py
+++ backend/run.py
@@ -3,6 +3,6 @@ from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+    socketio.run(app, host="0.0.0.0", port=port)
